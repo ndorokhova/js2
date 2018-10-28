2ade212ce6ffa7ff2d0e890191d3221814b39d2d
ndorokhova <dorokhova_n@mail.ru>
2018-10-28 13:26:50 +0300

    lesson-4

--- lesson-4/app.js
+++
@@ -1,8 +0,0 @@
-$('.control-top a').on('click', function(){  
-    $('.control-top a').removeClass('active');
-    $(this).addClass('active');
-
-    $('.control-main div').removeClass();
-    $('.control-main div').addClass('hidden');
-    $(('div #p-') + $(this)[0].id).addClass('show');   
-})
